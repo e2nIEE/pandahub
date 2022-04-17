3b04aef0e4e6d830764daef2d7304b6757a13698
Jannis Kupka <jannis.kupka@retoflow.de>
2022-04-17 12:25:21 +0200

    added support for python 3.7

--- setup.py
+++ setup.py
@@ -16,12 +16,13 @@ test_requirements = ['pytest>=3', ]
 setup(
     author="Jan Ulffers, Leon Thurner, Jannis Kupka, Mike Vogt, Joschka Thurner",
     author_email='info@pandapower.de',
-    python_requires='>=3.8',
+    python_requires='>=3.7',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
     ],
