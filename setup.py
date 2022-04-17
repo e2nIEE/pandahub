7ef2061675dc1aed71341f59a8b4ee2bdb218e53
Jannis Kupka <jannis.kupka@retoflow.de>
2022-04-17 12:21:54 +0200

    bumped version to 0.2.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md') as readme_file:
 
 with open('CHANGELOG.md', 'rb') as f:
     changelog = f.read().decode('utf-8')
-    
+
 with open('requirements.txt') as req_file:
     requirements = req_file.read()
 
@@ -35,7 +35,7 @@ setup(
     name='pandahub',
     packages=find_packages(),
     url='https://github.com/e2nIEE/pandahub',
-    version='0.1.3',
+    version='0.2.0',
     include_package_data=True,
     long_description_content_type='text/markdown',
     zip_safe=False,
