3dc52f96f4bc1ac72e48aa718a0ae849c003b4be
Joschka Thurner <git@joschkathurner.de>
2023-06-15 10:52:47 +0200

    compat for passing auth pars in URI

--- pandahub/api/internal/db.py
+++ pandahub/api/internal/db.py
@@ -7,10 +7,11 @@ from fastapi_users_db_mongodb.access_token import MongoDBAccessTokenDatabase
 from pandahub.api.internal import settings
 from pandahub.api.internal.models import AccessToken, UserDB
 
-client = motor.motor_asyncio.AsyncIOMotorClient(
-    host=settings.MONGODB_URL, username=settings.MONGODB_USER, password=settings.MONGODB_PASSWORD,
-    uuidRepresentation="standard"
-)
+mongo_client_args = {"host": settings.MONGODB_URL, "uuidRepresentation": "standard", "connect": False}
+if settings.MONGODB_USER:
+    mongo_client_args |= {"username": settings.MONGODB_USER, "password": settings.MONGODB_PASSWORD}
+
+client = motor.motor_asyncio.AsyncIOMotorClient(**mongo_client_args)
 
 client.get_io_loop = asyncio.get_event_loop
 
