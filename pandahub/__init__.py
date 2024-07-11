051232acffa24acb32783c43a9becde34e32fdc3
Joschka Thurner <git@joschkathurner.de>
2024-07-11 13:31:37 +0200

    remove PandaHubClient

--- pandahub/__init__.py
+++ pandahub/__init__.py
@@ -3,4 +3,3 @@ import importlib.metadata
 __version__ = importlib.metadata.version("pandahub")
 
 from pandahub.lib.PandaHub import PandaHub, PandaHubError
-from pandahub.client.PandaHubClient import PandaHubClient
