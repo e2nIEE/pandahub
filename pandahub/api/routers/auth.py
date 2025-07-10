bb0655c555ef35e61b68b9346013c5d81f4848ba
Joschka Thurner <git@joschkathurner.de>
2025-07-10 08:41:05 +0200

    remove user verification and password reset

--- pandahub/api/routers/auth.py
+++ pandahub/api/routers/auth.py
@@ -8,8 +8,6 @@ router = APIRouter(prefix="/auth", tags=["auth"])
 
 router.include_router(fastapi_users.get_auth_router(auth_backend))
 
-router.include_router(fastapi_users.get_reset_password_router())
-
 if REGISTRATION_ENABLED:
     router.include_router(
         fastapi_users.get_register_router(UserRead, UserCreate),
--- pandahub/api/routers/users.py
+++ pandahub/api/routers/users.py
@@ -1,6 +1,5 @@
 from fastapi import APIRouter
 
-from pandahub.api.internal import settings
 from pandahub.api.internal.schemas import UserRead, UserUpdate
 from pandahub.api.internal.users import fastapi_users
 
@@ -9,7 +8,4 @@ router = APIRouter(
     tags=["users"]
 )
 
-router.include_router(
-    fastapi_users.get_users_router(UserRead, UserUpdate,
-        requires_verification=settings.EMAIL_VERIFICATION_REQUIRED),
-)
+router.include_router(fastapi_users.get_users_router(UserRead, UserUpdate))
